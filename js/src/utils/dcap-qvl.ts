9d4077f207686d8aa258d761446a3962f44b4463
Coffee <zjchen1234@foxmail.com>
2025-10-29 16:52:08 +0800

    refactor

--- js/src/utils/dcap-qvl.ts
+++ js/src/utils/dcap-qvl.ts
@@ -3,6 +3,7 @@ export async function getDcapQvlUtils() {
     const { js_verify, js_get_collateral } = await import(
       '@phala/dcap-qvl-node'
     );
+
     return {
       jsVerify: js_verify,
       jsGetCollateral: js_get_collateral,
@@ -13,10 +14,13 @@ export async function getDcapQvlUtils() {
       js_get_collateral,
       default: init,
     } = await import('@phala/dcap-qvl-web');
+
     const { default: wasm } = await import(
       '@phala/dcap-qvl-web/dcap-qvl-web_bg.wasm'
     );
+
     await init({ module_or_path: wasm });
+
     return {
       jsVerify: js_verify,
       jsGetCollateral: js_get_collateral,
