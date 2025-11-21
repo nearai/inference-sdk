f0cba4ac1a3751e6fd577a1753ac35b924ca0cc2
Coffee <zjchen1234@foxmail.com>
2025-11-21 14:24:11 +0800

    fix: remove log

--- js/src/core/chat.ts
+++ js/src/core/chat.ts
@@ -16,10 +16,6 @@ export function verifySigningAddress(
   signingAddress: string,
   attestations: ModelAttestation[],
 ) {
-  console.log({
-    signingAddress,
-    attestations,
-  });
   const modelAttestation = attestations.find((attestation) => {
     return hexToBuffer(attestation.signing_address).equals(
       hexToBuffer(signingAddress),
